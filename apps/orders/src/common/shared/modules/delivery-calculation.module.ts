cb6522d07df92863948413e3847f18f9f5f1a59f
josesilveiraa07 <jose.silveira@sblabs.dev>
2025-02-28 12:02:22 -0300

    chore: add dotenv to gitignore

--- apps/orders/src/common/shared/modules/delivery-calculation.module.ts
+++ apps/orders/src/common/shared/modules/delivery-calculation.module.ts
@@ -1,5 +1,7 @@
-import { HttpModule } from '@nestjs/axios';
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import deliveryConfigProvider from '../../config/delivery.config';
 import { DeliveryCalculationService } from './delivery-calculation.service';
 import { MelhorEnvioDeliveryCalculationService } from './impl/melhorenvio-delivery-calculation.service';
 
@@ -8,7 +10,29 @@ import { MelhorEnvioDeliveryCalculationService } from './impl/melhorenvio-delive
   providers: [
     {
       provide: DeliveryCalculationService,
-      useClass: MelhorEnvioDeliveryCalculationService,
+      inject: [HttpService, deliveryConfigProvider.KEY],
+      useFactory: (
+        httpService: HttpService,
+        deliveryConfig: ConfigType<typeof deliveryConfigProvider>,
+      ) => {
+        const providers = {
+          melhorenvio: new MelhorEnvioDeliveryCalculationService(
+            httpService,
+            deliveryConfig,
+          ),
+        };
+
+        const provider = deliveryConfig.provider;
+
+        if (!providers[provider]) {
+          throw new Error(`Provider for ${provider} is not registered`);
+        }
+
+        console.log(providers[provider]);
+
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        return providers[provider];
+      },
     },
   ],
   exports: [DeliveryCalculationService],
